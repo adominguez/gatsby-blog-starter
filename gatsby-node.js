2bc1d481e4070e06e6de6371b4fef61b90d2c8ec
Corbin Crutchley <crutchcorn@gmail.com>
2020-03-17 18:59:46 -0700

    Get initial blog page list done

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,7 @@
 
 // You can delete this file if you're not using it
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`);
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
@@ -21,4 +22,43 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       value,
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const postList = path.resolve(`./src/templates/blog-list.jsx`)
+  return graphql(
+    `
+      {
+        allMarkdownRemark(limit: 1000) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              excerpt(pruneLength: 160)
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create blog posts pages.
+    const posts = result.data.allMarkdownRemark.edges.map(post => post.node)
+
+    createPage({
+      path: `/`,
+      component: postList,
+      context: {
+        posts,
+      },
+    })
+
+    return null
+  })
 }
